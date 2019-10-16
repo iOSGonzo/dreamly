72371f9a5a8958b4d630839c6d31b99f8bbb4da3
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-10-16 12:02:55 -0700

    got login screen to work with firebase auth

--- dreamly/LoginViewController.swift
+++ dreamly/LoginViewController.swift
@@ -8,10 +8,12 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     
@@ -21,8 +23,36 @@ class LoginViewController: UIViewController {
     }
 
     
-    @IBAction func loginPressed(_ sender: UIButton) {
+    @IBAction func registerPressed(_ sender: UIButton) {
         
+        if emailTextField.text != nil && passwordTextField.text != nil{
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (result, error) in
+                if error != nil{
+                    print("Error!:(")
+                }
+                else{
+                    let uid = result?.user.uid
+                    let ref = Database.database().reference(withPath: "users").child(uid!)
+                    ref.setValue(["email":self.emailTextField.text!])
+                    ref.setValue(["password":self.passwordTextField.text!])
+                }
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        if emailTextField.text != nil && passwordTextField.text != nil{
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (result, error) in
+                if error != nil{
+                    print("Error!:(")
+                }
+                else{
+                    //let uid = result?.user.uid
+                }
+            }
+        }
     }
     
 }
