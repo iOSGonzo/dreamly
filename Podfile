21607beaf2191e6627868a211ef18a8681eb1a5e
Gonzalo Birrueta <birruetabusiness@gmail.com>
2019-10-16 11:41:01 -0700

    installed firebase auth, starting login view

--- Podfile
+++ Podfile
@@ -7,6 +7,8 @@ target 'dreamly' do
 
   # Pods for dreamly
 
-pod 'Firebase/Analytics'
-
+	pod 'Firebase/Analytics'
+	pod 'Firebase/Auth'
+	pod 'Firebase/Core'
+	pod 'Firebase/Database'
 end
---
+++ dreamly/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  dreamly
+//
+//  Created by Gonzalo Birrueta on 10/16/19.
+//  Copyright © 2019 Gonzalo Birrueta. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+    }
+    
+}
